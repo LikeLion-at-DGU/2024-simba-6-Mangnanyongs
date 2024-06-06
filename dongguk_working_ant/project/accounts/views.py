d923a21931497b263244e325e3d6d1a81964761a
jiyoon607 <6judy611@gmail.com>
2024-06-06 23:59:26 +0900

    Feat: 회원가입 기능 완료

--- dongguk_working_ant/project/accounts/views.py
+++ dongguk_working_ant/project/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
+from .models import Profile_Staff, Profile_Student
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -23,3 +24,57 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('main:mainpage')
+
+def signup_choose(request):
+    return render(request, 'accounts/signup_choose.html')
+
+def signup_student(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['confirm']:
+            user = User.objects.create_user(
+                username = request.POST['username'],
+                password = request.POST['password'],
+            )
+
+            name = request.POST['name']
+            gender = request.POST['gender']
+            birth = request.POST['birth']
+            department = request.POST['department']
+            phone = request.POST['phone']
+            email = request.POST['email']
+            if request.FILES.get('certification'):
+                certification = request.FILES['certification']
+
+            profile_student = Profile_Student(user=user, name=name, gender=gender, birth=birth, department=department, phone=phone, email=email)
+            if request.FILES.get('certification'):
+                profile_student = Profile_Student(certification=certification)
+            profile_student.save()
+
+            auth.login(request, user)
+            return redirect('/')
+
+    return render(request, 'accounts/signup_student.html')
+
+def signup_staff(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['confirm']:
+            user = User.objects.create_user(
+                username = request.POST['username'],
+                password = request.POST['password'],
+            )
+
+            name = request.POST['name']
+            email = request.POST['email']
+            if request.FILES.get('certification'):
+                certification = request.FILES['certification']
+
+            profile_staff = Profile_Staff(user=user, name=name, email=email)
+            if request.FILES.get('certification'):
+                profile_staff = Profile_Staff(certification=certification)
+
+            profile_staff.save()
+
+            auth.login(request, user)
+            return redirect('/')
+
+    return render(request, 'accounts/signup_staff.html')

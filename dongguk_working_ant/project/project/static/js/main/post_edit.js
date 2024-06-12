3da58a36612cb07d97a297dae908d001a9d4f3ca
jjjung0921 <earthenergy-1128@naver.com>
2024-06-12 10:33:18 +0900

    feat: 화면 크기 조정 필요

--- dongguk_working_ant/project/project/static/js/main/post_edit.js
+++ dongguk_working_ant/project/project/static/js/main/post_edit.js
@@ -13,3 +13,15 @@
         });
     });
 });*/
+
+document.getElementById('add_question').addEventListener('click', function() {
+    let obj = document.getElementById('main3');
+    let qborder = document.querySelector('.q_border'); // 첫 번째 q_border 요소 선택
+    let newDiv = qborder.cloneNode(true); // 전체 내용을 복사
+    
+    // 새로운 질문 번호 설정
+    let questionNumber = obj.querySelectorAll('.q_border').length + 1;
+    newDiv.querySelector('.orange').textContent = questionNumber + '.';
+
+    obj.insertBefore(newDiv, document.getElementById('add_question'));
+});

90f43617a1b30171bbe5b8d8052f937fab019e10
nicerjs23 <nicerjs23@gmail.com>
2024-06-14 13:24:37 +0900

    Feat:staff_mypage개발중

--- dongguk_working_ant/project/users/urls.py
+++ dongguk_working_ant/project/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import *
+# from users import views #추가한코드
 
 app_name = "users"
 urlpatterns = [
-    path('mypage/', mypage, name="mypage"),
+    path('staff_mypage/', staff_mypage, name="staff_mypage"),
 ]
